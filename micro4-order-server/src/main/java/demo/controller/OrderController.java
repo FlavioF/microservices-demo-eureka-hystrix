0e9f2b66cbe1c6a460295e02e3201bc7a73f7ecd
Flávio Ferreira <fferreira@bikeemotion.com>
2017-01-05 18:53:13 +0000

    Changed layout

--- micro4-order-server/src/main/java/demo/controller/OrderController.java
+++ micro4-order-server/src/main/java/demo/controller/OrderController.java
@@ -15,21 +15,19 @@ public class OrderController {
 	@Autowired
 	OrderService orderService;
 	
-	
 	/**
 	 * Display a small list of Sentences to the caller:
 	 */
 	@RequestMapping("/order")
 	public @ResponseBody String getSentence() {
 		long start = System.currentTimeMillis();
-		String output = 
-			"<h1>Happy Meals at MacDonalds</h1><br/>" +
-			orderService.build() + "<br/><br/>" +
-			orderService.build() + "<br/><br/>" +
-			orderService.build() + "<br/><br/>" +
-			orderService.build() + "<br/><br/>" +
-			orderService.build() + "<br/><br/>" +
-			"<img src='http://www.nogarlicnoonions.com/images/uploads/NEWS/Mcdonalds.jpg' width=533 height=300>";
+		String output =
+				"<h1>Happy Meals <img src='http://www.mcdonalds.co.uk/content/dam/McDonaldsUK/Food-category-header.png' width=200>at MacDonalds!</h1><br/><font color=navy size=4>" +
+						orderService.build() + "!</font><br/><br/><font color=black size=6>" +
+						orderService.build() + "!</font><br/><br/><font color=darkbrown size=5>" +
+						orderService.build() + "!</font><br/><br/><font color=teal size=4>" +
+						orderService.build() + "!</font><br/><br/><font color=orange size=6>" +
+						orderService.build() + "!</font><br/><br/><img src='http://www.nogarlicnoonions.com/images/uploads/NEWS/Mcdonalds.jpg' width=533 height=300>";
 		long end = System.currentTimeMillis();
 		return output + "<br><hr><font size=small>Elapsed time (ms): " + (end - start) + "</font>";
 	}
